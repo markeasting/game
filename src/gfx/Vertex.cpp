0f43e2d4ff2a513a2484d6f162ff409d4df7b7df
markeasting <markeasting@gmail.com>
2023-03-08 21:40:18 +0100

    Added uv property to Vertex + set glVertexAttribPointer

--- src/gfx/Vertex.cpp
+++ src/gfx/Vertex.cpp
@@ -4,8 +4,16 @@
 
 Vertex::Vertex() {}
 
-Vertex::Vertex(glm::vec3 position, glm::vec3 normal, glm::vec4 color)
-    : position(position), normal(normal), color(color)
+// Vertex::Vertex(glm::vec3 position)
+//     : position(position)
+// {}
+
+// Vertex::Vertex(glm::vec3 position, glm::vec3 normal)
+//     : position(position), normal(normal)
+// {}
+
+Vertex::Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 uv)
+    : position(position), normal(normal), uv(uv)
 {}
 
 Vertex::Vertex(float x, float y, float z)
@@ -15,8 +23,10 @@ Vertex::Vertex(float x, float y, float z)
 void Vertex::setAttribPointers() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) offsetof(Vertex, position));
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) offsetof(Vertex, normal));
-    glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) offsetof(Vertex, color));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) offsetof(Vertex, uv));
+    // glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) offsetof(Vertex, color));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
     glEnableVertexAttribArray(2);
+    // glEnableVertexAttribArray(3);
 }
--- src/gfx/Vertex.h
+++ src/gfx/Vertex.h
@@ -4,12 +4,15 @@
 
 struct Vertex {
     
-    glm::vec3 position = glm::vec3(0.0f);
-    glm::vec3 normal   = glm::vec3(0.0f);
-    glm::vec4 color    = glm::vec4(0.0f);
+    glm::vec3 position = glm::vec3(0);
+    glm::vec3 normal   = glm::vec3(0);
+    glm::vec2 uv       = glm::vec2(0);
+    // glm::vec4 color    = glm::vec4(0);
 
     Vertex();
-    Vertex(glm::vec3 position = glm::vec3(0.0f), glm::vec3 normal = glm::vec3(0.0f), glm::vec4 color = glm::vec4(0.0f));
+    // Vertex(glm::vec3 position);
+    // Vertex(glm::vec3 position, glm::vec3 normal);
+    Vertex(glm::vec3 position = glm::vec3(0), glm::vec3 normal = glm::vec3(0), glm::vec2 uv = glm::vec2(0));
     Vertex(float x = 0, float y = 0, float z = 0);
 	~Vertex() = default;
 
