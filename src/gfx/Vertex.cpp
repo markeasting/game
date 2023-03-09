2c5cb10ed27e1a7a0acba6548082eb0874189f09
markeasting <markeasting@gmail.com>
2023-03-09 21:13:00 +0100

    Updated Vertex setAttribPointers()

--- src/gfx/Vertex.cpp
+++ src/gfx/Vertex.cpp
@@ -4,14 +4,6 @@
 
 Vertex::Vertex() {}
 
-// Vertex::Vertex(glm::vec3 position)
-//     : position(position)
-// {}
-
-// Vertex::Vertex(glm::vec3 position, glm::vec3 normal)
-//     : position(position), normal(normal)
-// {}
-
 Vertex::Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 uv)
     : position(position), normal(normal), uv(uv)
 {}
@@ -21,12 +13,11 @@ Vertex::Vertex(float x, float y, float z)
 {}
 
 void Vertex::setAttribPointers() {
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) offsetof(Vertex, position));
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) offsetof(Vertex, normal));
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) offsetof(Vertex, uv));
-    // glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*) offsetof(Vertex, color));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, position));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, normal));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, uv));
+    
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
     glEnableVertexAttribArray(2);
-    // glEnableVertexAttribArray(3);
 }
