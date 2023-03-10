367489ea92cb2bfd14c5c5d95630c254ffa6caf3
markeasting <markeasting@gmail.com>
2023-03-10 17:18:19 +0100

    WIP: use new Mesh constructor. TODO: port over old PrimitiveMeshes to Geometry classes

--- src/geom/PrimitiveMeshes.h
+++ src/geom/PrimitiveMeshes.h
@@ -2,24 +2,24 @@
 
 #include "geom/Mesh.h"
 
-struct TetrahedronMesh : public Mesh {
-    TetrahedronMesh(float size = 1.0f);
-};
+// struct TetrahedronMesh : public Mesh {
+//     TetrahedronMesh(float size = 1.0f);
+// };
 
-struct BoxMesh : public Mesh {
-    BoxMesh(float size = 1.0f);
-    BoxMesh(float width, float height, float depth);
-};
+// struct BoxMesh : public Mesh {
+//     BoxMesh(float size = 1.0f);
+//     BoxMesh(float width, float height, float depth);
+// };
 
-struct PlaneMesh : public Mesh {
-    PlaneMesh(float size = 1.0f);
-    PlaneMesh(float length, float width);
-};
+// struct PlaneMesh : public Mesh {
+//     PlaneMesh(float size = 1.0f);
+//     PlaneMesh(float length, float width);
+// };
 
-struct SphereMesh : public Mesh {
-    SphereMesh(float size = 1.0f, unsigned int segments = 20);
-};
+// struct SphereMesh : public Mesh {
+//     SphereMesh(float size = 1.0f, unsigned int segments = 20);
+// };
 
-struct ArrowMesh : public Mesh {
-    ArrowMesh(float size = 1.0f);
-};
+// struct ArrowMesh : public Mesh {
+//     ArrowMesh(float size = 1.0f);
+// };
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "app/Game.h"
 #include "geom/Mesh.h"
 #include "geom/PrimitiveMeshes.h"
+#include "geom/BoxGeometry.h"
 
 /* Initialize global events class */
 EventEmitter Events;
@@ -29,36 +30,36 @@ int main() {
 
     colorMaterial.setUniform("u_color", glm::vec4(0.0f, 0.0f, 0.8f, 1.0f));
 
-    auto floor = ref<PlaneMesh>(10.0f);
-    floor->setRotation({ -90.0f, 0.0f, 0.0f });
-    floor->setMaterial(phongMaterial);
-    game.m_renderer.add(floor);
+    // auto floor = ref<PlaneMesh>(10.0f);
+    // floor->setRotation({ -90.0f, 0.0f, 0.0f });
+    // floor->setMaterial(phongMaterial);
+    // game.m_renderer.add(floor);
 
-    auto myMesh = ref<BoxMesh>(0.5f);
+    auto myMesh = ref<Mesh>(BoxGeometry(0.5f), colorMaterial);
     myMesh->setPosition({ 2.0f, 0.5f, 0.0f });
     myMesh->setMaterial(colorMaterial);
     game.m_renderer.add(myMesh);
 
-    auto tetra = ref<TetrahedronMesh>(1.0f);
-    tetra->setPosition({ 0.0f, 1.0f, 0.0f });
-    tetra->setMaterial(colorMaterial);
-    game.m_renderer.add(tetra);
+    // auto tetra = ref<TetrahedronMesh>(1.0f);
+    // tetra->setPosition({ 0.0f, 1.0f, 0.0f });
+    // tetra->setMaterial(colorMaterial);
+    // game.m_renderer.add(tetra);
 
-    tetra->add(myMesh);
+    // tetra->add(myMesh);
 
-    auto overlay = ref<PlaneMesh>(2.0f);
-    Material textureMaterial = Material("/shader/Basic.vert", "/shader/BasicTextured.frag");
-    textureMaterial.assignTexture("");
-    overlay->setMaterial(textureMaterial);
-    overlay->setPosition({ 0, -0.5f, 0 });
-    overlay->setScale({ 0.3f, 0.3f, 0 });
-    overlay->m_useProjectionMatrix = false;
-    game.m_renderer.add(overlay);
+    // auto overlay = ref<PlaneMesh>(2.0f);
+    // Material textureMaterial = Material("/shader/Basic.vert", "/shader/BasicTextured.frag");
+    // textureMaterial.assignTexture("");
+    // overlay->setMaterial(textureMaterial);
+    // overlay->setPosition({ 0, -0.5f, 0 });
+    // overlay->setScale({ 0.3f, 0.3f, 0 });
+    // overlay->m_useProjectionMatrix = false;
+    // game.m_renderer.add(overlay);
 
     while (game.isRunning()) {
         game.update();
 
-        tetra->setRotation({ game.getTime() * 100.0f, 0.0f, 0.0f });
+        // tetra->setRotation({ game.getTime() * 100.0f, 0.0f, 0.0f });
 
         float oscillator = sin(game.getTime() * 1.5) / 2.0f + 0.5f;
         colorMaterial.setUniform("u_color", glm::vec4(0.0f, oscillator, 0.8f, 1.0f));
