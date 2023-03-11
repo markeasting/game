b078446d036d2484f0a43fed12b4c59a9a8f7594
Mark <markeasting@gmail.com>
2023-03-11 14:57:01 +0100

    Updated main with new Mesh constructor + geometry classes

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 #include "common_includes.h"
 
 #include "app/Game.h"
-#include "geom/Mesh.h"
-#include "geom/PrimitiveMeshes.h"
-#include "geom/BoxGeometry.h"
+#include "geom/index.h"
+#include "gfx/Mesh.h"
 
 /* Initialize global events class */
 EventEmitter Events;
@@ -15,9 +14,10 @@ int main() {
 
     auto lightDirection = ref<Uniform<glm::vec3>>("u_lightDirection", glm::vec3(0.5f, 0.0f, 2.0f));
 
-    Material colorMaterial = Material("/shader/Color");
+    Material colorMaterial = Material("Color");
+    Material phongMaterial = Material("Phong", { lightDirection });
     
-    Material phongMaterial = Material("/shader/Phong", {
+    Material phongMaterial2 = Material("Phong", {
         ref<Uniform<glm::vec3>>("ambient", glm::vec3(0.2f, 0.3f, 0.3f)),
         ref<Uniform<glm::vec3>>("diffuseAlbedo", glm::vec3(0.2f, 0.3f, 0.3f)),
         ref<Uniform<glm::vec3>>("specularAlbedo", glm::vec3(0.2f, 0.2f, 0.2f)),
@@ -30,36 +30,37 @@ int main() {
 
     colorMaterial.setUniform("u_color", glm::vec4(0.0f, 0.0f, 0.8f, 1.0f));
 
-    // auto floor = ref<PlaneMesh>(10.0f);
-    // floor->setRotation({ -90.0f, 0.0f, 0.0f });
-    // floor->setMaterial(phongMaterial);
-    // game.m_renderer.add(floor);
+    auto floor = ref<Mesh>(PlaneGeometry(10.0f), phongMaterial2);
+    floor->setRotation({ -90.0f, 0.0f, 0.0f });
+    game.m_renderer.add(floor);
 
-    auto myMesh = ref<Mesh>(BoxGeometry(0.5f), colorMaterial);
-    myMesh->setPosition({ 2.0f, 0.5f, 0.0f });
-    myMesh->setMaterial(colorMaterial);
-    game.m_renderer.add(myMesh);
+    auto sphere = ref<Mesh>(SphereGeometry(1.0f), phongMaterial);
+    sphere->setPosition({ 0.0f, 0.5f, 3.0f });
+    game.m_renderer.add(sphere);
 
-    // auto tetra = ref<TetrahedronMesh>(1.0f);
-    // tetra->setPosition({ 0.0f, 1.0f, 0.0f });
-    // tetra->setMaterial(colorMaterial);
-    // game.m_renderer.add(tetra);
+    auto box = ref<Mesh>(BoxGeometry(0.5f), colorMaterial);
+    box->setPosition({ 2.0f, 0.5f, 0.0f });
+    game.m_renderer.add(box);
 
-    // tetra->add(myMesh);
+    auto tetra = ref<Mesh>(TetrahedronGeometry(1.0f), colorMaterial);
+    tetra->setPosition({ 0.0f, 1.0f, 0.0f });
+    game.m_renderer.add(tetra);
 
-    // auto overlay = ref<PlaneMesh>(2.0f);
-    // Material textureMaterial = Material("/shader/Basic.vert", "/shader/BasicTextured.frag");
-    // textureMaterial.assignTexture("");
-    // overlay->setMaterial(textureMaterial);
-    // overlay->setPosition({ 0, -0.5f, 0 });
-    // overlay->setScale({ 0.3f, 0.3f, 0 });
-    // overlay->m_useProjectionMatrix = false;
-    // game.m_renderer.add(overlay);
+    tetra->add(box);
+
+    auto overlay = ref<Mesh>(PlaneGeometry(2.0f));
+    Material textureMaterial = Material("Basic.vert", "BasicTextured.frag");
+    textureMaterial.assignTexture("");
+    overlay->setMaterial(textureMaterial);
+    overlay->setPosition({ 0, -0.5f, 0 });
+    overlay->setScale({ 0.3f, 0.3f, 0 });
+    overlay->m_useProjectionMatrix = false;
+    game.m_renderer.add(overlay);
 
     while (game.isRunning()) {
         game.update();
 
-        // tetra->setRotation({ game.getTime() * 100.0f, 0.0f, 0.0f });
+        tetra->setRotation({ game.getTime() * 100.0f, 0.0f, 0.0f });
 
         float oscillator = sin(game.getTime() * 1.5) / 2.0f + 0.5f;
         colorMaterial.setUniform("u_color", glm::vec4(0.0f, oscillator, 0.8f, 1.0f));
