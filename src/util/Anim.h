81504b4d71930dcf341b6863932d261c66832596
markeasting <markeasting@gmail.com>
2023-03-19 19:57:30 +0100

    Added lerp util

---
+++ src/util/Anim.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// #include <algorithm>
+#include <cmath>
+
+namespace Anim {
+    void lerp(float& value, float target, float factor = 0.98) {
+        value = std::lerp(value, target, factor);
+    }
+}
