6adae6802e2d03be03a02c008238a9d0c69912f6
Mark <markeasting@gmail.com>
2023-04-11 14:03:17 +0200

    Added AABB and Plane classes

---
+++ src/physics/AABB.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "common.h"
+#include "physics/Plane.h"
+
+class AABB {
+public:
+
+    vec3 min = vec3(-std::numeric_limits<float>::infinity());
+    vec3 max = vec3(std::numeric_limits<float>::infinity());
+
+    AABB() = default;
+
+	inline void set(vec3 _min, vec3 _max) {
+		min = _min;
+		max = _max;
+	}
+
+    inline void expandByScalar(float scalar) {
+        min -= scalar;
+        max += scalar;
+    }
+
+    inline bool containsPoint(vec3 point) {
+        return point.x < min.x || point.x > max.x ||
+            point.y < min.y || point.y > max.y ||
+            point.z < min.z || point.z > max.z ? false : true;
+    }
+
+    inline bool intersects(AABB other) {
+        return other.max.x < min.x || other.min.x > max.x ||
+            other.max.y < min.y || other.min.y > max.y ||
+            other.max.z < min.z || other.min.z > max.z ? false : true;
+    }
+
+    inline bool intersectsPlane(Plane plane) {
+        float _min, _max;
+
+		if ( plane.normal.x > 0 ) {
+			_min = plane.normal.x * min.x;
+			_max = plane.normal.x * max.x;
+		} else {
+			_min = plane.normal.x * max.x;
+			_max = plane.normal.x * min.x;
+		}
+
+		if ( plane.normal.y > 0 ) {
+			_min += plane.normal.y * min.y;
+			_max += plane.normal.y * max.y;
+		} else {
+			_min += plane.normal.y * max.y;
+			_max += plane.normal.y * min.y;
+		}
+
+		if ( plane.normal.z > 0 ) {
+			_min += plane.normal.z * min.z;
+			_max += plane.normal.z * max.z;
+		} else {
+			_min += plane.normal.z * max.z;
+			_max += plane.normal.z * min.z;
+		}
+
+		return ( _min <= - plane.constant && _max >= - plane.constant );
+    }
+
+};
---
+++ src/physics/Plane.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "common.h"
+
+class Plane {
+public:
+    
+    vec3 normal = { 0, 1.0f, 0 };
+    float constant = 0.0f;
+
+	Plane() = default;
+    Plane(vec3 normal, float constant)
+        : normal(normal), constant(constant) {};
+
+	void normalize() {
+
+		const float invLen = 1.0f / normal.length();
+		
+        normal *= invLen;
+		constant *= invLen;
+
+	}
+
+	float distanceToPoint(vec3 point) {
+
+		return glm::dot(normal, point) + constant;
+
+	}
+
+    /* @TODO TEST */
+	vec3 projectPoint(vec3 point) {
+		return point + (normal * -Plane::distanceToPoint(point));
+	}
+
+    // float distanceToSphere( sphere ) {
+	// 	return distanceToPoint( sphere.center ) - sphere.radius;
+	// }
+
+};
