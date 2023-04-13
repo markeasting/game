996c80a91dc133bb860dcebb9dceaa78fb715fa7
Mark <markeasting@gmail.com>
2023-04-13 16:44:21 +0200

    Added y-up argument for PlaneGeometry

--- src/geom/PlaneGeometry.h
+++ src/geom/PlaneGeometry.h
@@ -5,24 +5,25 @@
 class PlaneGeometry : public Geometry {
 public:
 
-    PlaneGeometry(float size = 1.0f) {
-        this->generate(size, size);
+    PlaneGeometry(float size = 1.0f, bool yUp = false) {
+        this->generate(size, size, yUp);
     };
-    PlaneGeometry(float width, float height) {
-        this->generate(width, height);
+    PlaneGeometry(float width, float height, bool yUp = false) {
+        this->generate(width, height, yUp);
     };
 
 private:
-    void generate(float width, float height) {
+    /* Default to z-up */
+    void generate(float width, float height, bool yUp = false) {
 
         float x = 0.5f * width;
         float y = 0.5f * height;
         
         std::vector<Vertex> vertices {
-            Vertex(vec3(  x, -y, 0 ), vec3( 0, 0, 1 ), vec2( 1, 0 )),
-            Vertex(vec3(  x,  y, 0 ), vec3( 0, 0, 1 ), vec2( 1, 1 )),
-            Vertex(vec3( -x, -y, 0 ), vec3( 0, 0, 1 ), vec2( 0, 0 )),
-            Vertex(vec3( -x,  y, 0 ), vec3( 0, 0, 1 ), vec2( 0, 1 )),
+            yUp ? Vertex(vec3(  x, 0, y ), vec3( 0, 1, 0 ), vec2( 1, 0 )) : Vertex(vec3(  x, -y, 0 ), vec3( 0, 0, 1 ), vec2( 1, 0 )),
+            yUp ? Vertex(vec3(  x, 0, -y ), vec3( 0, 1, 0 ), vec2( 1, 1 )) : Vertex(vec3(  x,  y, 0 ), vec3( 0, 0, 1 ), vec2( 1, 1 )),
+            yUp ? Vertex(vec3( -x, 0, y ), vec3( 0, 1, 0 ), vec2( 0, 0 )) : Vertex(vec3( -x, -y, 0 ), vec3( 0, 0, 1 ), vec2( 0, 0 )),
+            yUp ? Vertex(vec3( -x, 0, -y ), vec3( 0, 1, 0 ), vec2( 0, 1 )) : Vertex(vec3( -x,  y, 0 ), vec3( 0, 0, 1 ), vec2( 0, 1 )),
         };
 
         std::vector<unsigned int> indices = {
