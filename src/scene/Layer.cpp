cd30de02f5146bfe49e110a8b5ddbda92113f550
Mark <markeasting@gmail.com>
2023-11-22 20:58:07 +0100

    Also add non-convex meshes to debug renderer

--- src/scene/Layer.cpp
+++ src/scene/Layer.cpp
@@ -29,7 +29,7 @@ void Layer::add(Ref<RigidBody> body) {
 
     this->add(body->mesh);
 
-    if (body->collider->m_type == ColliderType::CONVEX_MESH) {
+    if (body->collider->m_type == ColliderType::CONVEX_MESH || body->collider->m_type == ColliderType::INEFFICIENT_MESH) {
         const auto MC = std::static_pointer_cast<MeshCollider>(body->collider);
 
         this->add(MC->m_mesh);
